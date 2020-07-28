8e9175503e1b78f4c858dc17b1ba0950e0725982
Anne Culpepper <anne.culpepper@gmail.com>
2020-07-28 15:17:01 -0500

    edited h1 to personalize this tutorial

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 class Hello extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello there!</h1>
-				<h1>Hello there!</h1>
-				<h1>Hello there!</h1>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <h1>Anne Culpepper</h1>
+        <p>...is a beast</p>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
